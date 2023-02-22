6dc2dfbca45e7d4e06270391ca056e890ffecb80
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-23 00:04:18 +0600

    class.js

---
+++ class.js
@@ -0,0 +1,22 @@
+//ES6--synthetic sugar
+//1  class variable name should be uppercase
+//2  property should use; and define value =
+
+
+class Instructor{
+    name;
+    designation ='web course instructor'
+    team='web team'
+    location;
+    startSupportSession(time){
+     console.log(`start the support session at ${time}`)
+    }
+    createQuiz(module){
+     console.log(`please create quiz for ${module}`)
+    }
+
+}
+const Jannat = new Instructor();
+console.log(Jannat);
+Jannat.startSupportSession('9.00');
+Jannat.createQuiz(60);

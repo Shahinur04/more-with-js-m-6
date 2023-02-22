987b7ff52cb31e6f08febb738bc097f4c67eac08
Shahinur Islam <shahinur04042002@gmail.com>
2023-02-22 15:17:18 +0600

    chain.js

---
+++ chain.js
@@ -0,0 +1,15 @@
+//object er maje array
+const user=[{id:96,name:'Shahinur',job:'developer'}];
+console.log(user[0].job)
+
+const data={
+   count:5000,
+   data: [
+{id:1,name:'jannat',job:'love'},
+{id:2,name:'jannat1',job:'love1'}
+    ]
+}
+// console.log(data.data[0])
+
+const objectArrow=data?.data;
+console.log(objectArrow)
